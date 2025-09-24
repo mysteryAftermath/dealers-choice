e9097a538d3cf4ef511ed21d9431791d9f521cff
mysteryaftermath <mysteryaftermath@protonmail.com>
2025-09-24 17:27:48 -0400

    Refine Turnabout behavior and re-word Turnabout description for clarification.

--- noxious-balatro.lua
+++ noxious-balatro.lua
@@ -14,29 +14,35 @@ SMODS.Joker {
 	loc_txt = {
 		name = 'Turnabout',
 		text = {
-			"This Joker Give {C:blue}+1{} Hand for each",
-			"remaining hand at end of the last round",
-            "Resets when {C:attention}Boss Blind{} is defeated",
-			"{C:inactive}(Currently {C:chips}+#1#{C:inactive} Hands)"
+			"When {C:attention}Small Blind{} or {C:attention}Big Blind{}",
+			"is defeated, gain any remaining",
+            "hands when selecting next blind",
+			"{C:inactive}(Will give {C:blue}+#1#{C:inactive} Hands next blind)"
 		}
 	},
-	config = { extra = { hands = 0 } },
+	config = { extra = { hands = 0, hands_last = 0 } },
 	rarity = 2,
 	atlas = 'noxious-balatro',
 	pos = { x = 0, y = 0 },
 	cost = 7,
 	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.hands } }
+		return { vars = { card.ability.extra.hands, card.ability.extra.hands_last } }
 	end,
 	calculate = function(self, card, context)
-        if context.setting_blind then
+        if context.setting_blind and card.ability.extra.hands > 0 then
+			ease_hands_played(card.ability.extra.hands)
+			card.ability.extra.hands_last = card.ability.extra.hands
+			card.ability.extra.hands = 0
             return {
-                ease_hands_played(card.ability.extra.hands)
-            }
+				message = '+' .. tostring(card.ability.extra.hands_last) .. ' Hands',
+				colour = G.C.FILTER,
+				card = card
+			}
         end
 
         if context.end_of_round and context.cardarea == G.jokers and not G.GAME.blind.boss then
             card.ability.extra.hands = G.GAME.current_round.hands_left
+			ease_hands_played(-G.GAME.current_round.hands_left)
             return {
                 message = 'Hold It!',
 				colour = G.C.BLUE,
@@ -44,8 +50,8 @@ SMODS.Joker {
             }
         end
 
-        if context.end_of_round and context.cardarea == G.jokers and G.GAME.blind.boss then
-            card.ability.extra.hands = 0
+		if context.end_of_round and context.cardarea == G.jokers and G.GAME.blind.boss then
+			card.ability.extra.hands_last = 0
             return {
                 message = 'Adjourned!',
 				colour = G.C.FILTER,
